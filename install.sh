5adfeb93ee185e0c6c4978c895ff1701c8ea1653
Jaylen Douglas <hello@jaylendouglas.com>
2025-03-21 02:05:20 -0400

    removed install.sh file.

--- install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# python -m pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/nightly/directml
-# For Windows
-python -m pip install torch-directml
-python -m pip install diffusers transformers accelerate safetensors
